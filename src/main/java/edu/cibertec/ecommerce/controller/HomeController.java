4b245b5166c4508e37c76b364a134df47e25acd4
paul <pyupanquiv@hotmail.com>
2023-11-26 21:45:36 -0500

    Vista Productos v2

--- src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
+++ src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
@@ -1,29 +1,26 @@
 package edu.cibertec.ecommerce.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import edu.cibertec.ecommerce.model.Producto;
+
 import edu.cibertec.ecommerce.service.ProductoService;
 
 @Controller
-@RequestMapping("/administrador")
-public class AdministradorController {
+@RequestMapping("/")
+public class HomeController {
 
 	@Autowired
 	private ProductoService productoService;
 	
 	@GetMapping("")
 	public String home(Model model) {
+		model.addAttribute("productos",productoService.findAll());
 		
-		List<Producto> productos = productoService.findAll();
-		model.addAttribute("productos",productos);
-		
-		return "administrador/home";
+		return "usuario/home";
 	}
+	
 }
