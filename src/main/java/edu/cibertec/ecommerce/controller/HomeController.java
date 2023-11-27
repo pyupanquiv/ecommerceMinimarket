9accd705fd343e40c89187398c9f259a30160ba2
paul <pyupanquiv@hotmail.com>
2023-11-27 01:07:49 -0500

    Vista Productos v3

--- src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
+++ src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
@@ -1,26 +1,73 @@
 package edu.cibertec.ecommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-
+import edu.cibertec.ecommerce.model.DetallePedido;
+import edu.cibertec.ecommerce.model.Pedido;
+import edu.cibertec.ecommerce.model.Producto;
 import edu.cibertec.ecommerce.service.ProductoService;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 
+	private final Logger log= LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;
+	
+	//para almacenar los detalles de la orden
+	List<DetallePedido> detalles= new ArrayList<DetallePedido>();
+	
+	//Detalle del pedido
+	Pedido pedido = new Pedido();
+	
 	
 	@GetMapping("")
 	public String home(Model model) {
 		model.addAttribute("productos",productoService.findAll());
 		
 		return "usuario/home";
+	}
+	
+	@GetMapping("/productohome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model) {
+		log.info("id PRODUCTO enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+		
+		model.addAttribute("producto", producto);
+		
+		
+		return "usuario/productohome";
+	}
+	
+	@PostMapping("/cart")
+	public String addCart(@RequestParam Integer id, @RequestParam Integer cantidad) {
+		
+		DetallePedido detallePedido = new DetallePedido();
+		Producto producto = new Producto();
+		double sumaTotal = 0;
+		
+		Optional<Producto> optionalProducto = productoService.get(id);
+		log.info("Producto añadido: {}", optionalProducto.get());
+		log.info("Cantidad {}", cantidad);
+		
+		return "usuario/carrito";
 	}
 	
 }
