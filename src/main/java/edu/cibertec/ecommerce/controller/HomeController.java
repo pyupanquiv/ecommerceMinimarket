674ab8dd01ba05151fbe15858763c1ffeb6d4120
paul <pyupanquiv@hotmail.com>
2023-12-08 20:24:42 -0500

    Ultima Version 08122023

--- src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
+++ src/main/java/edu/cibertec/ecommerce/controller/HomeController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import edu.cibertec.ecommerce.model.DetallePedido;
 import edu.cibertec.ecommerce.model.Pedido;
